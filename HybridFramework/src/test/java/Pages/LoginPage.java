ee552b0991a2506b113c13937db03b608ed2e71f
lenovo <lenovo@DESKTOP-F9BB1G3>
2019-12-11 09:49:14 +0530

    New changes added

---
+++ HybridFramework/src/test/java/Pages/LoginPage.java
@@ -0,0 +1,15 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+	//Reusables reuse = new Reusables();
+	WebDriver driver;
+	
+	public void loginPage(){
+		driver = new ChromeDriver();
+		
+	}
+
+}
---
+++ HybridFramework/src/test/java/Pages/Resusables.java
@@ -0,0 +1,38 @@
+package Pages;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Resusables {
+
+	WebDriver driver;
+	Properties prop;
+	public void browerSetup(){
+		System.setProperty("webdriver.chrome.driver", "E:\\SeleniumProjects\\HybridFramework\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(readProperties("appURL"));
+	}
+	
+	
+	
+	public String  readProperties(String property){
+		//---------------Setting the file path--------> 
+		File file = new File("E:\\SeleniumProjects\\HybridFramework\\ObjectRepository\\ObjectRepository.properties");
+		//----------Converting into byte to read the file-------------------------->////
+		try{
+			FileInputStream inputstream = new FileInputStream(file);
+             prop = new Properties();
+             prop.load(inputstream);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return property;
+	}
+	
+}
